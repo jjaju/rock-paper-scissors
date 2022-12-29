49cafb241c1f73bf0ffb580dce29707a3a1dab8c
jung-m <markiyoung97@gmail.com>
2022-12-29 02:49:00 +0100

    Code clean up.

--- sketch.js
+++ sketch.js
@@ -2,13 +2,11 @@ const SCREEN_WIDTH = window.innerWidth;
 const SCREEN_HEIGHT = window.innerHeight;
 
 var handType = { ROCK: 0, PAPER: 1, SCISSOR: 2 };
-var initialCentroids = [];
 var rocks = [];
 var papers = [];
 var scissors = [];
 var allHands = [rocks, papers, scissors];
 var hands = [];
-var radius = 200;
 
 var rockIcon;
 var paperIcon;
@@ -20,17 +18,10 @@ function setup() {
     scissorIcon = loadImage("./assets/scissorTransp.png");
     createCanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
     background(0);
-    initialCentroids.push(createVector(SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3));
-    initialCentroids.push(
-        createVector((SCREEN_WIDTH * 2) / 3, SCREEN_HEIGHT / 3)
-    );
-    initialCentroids.push(
-        createVector(SCREEN_WIDTH / 2, (SCREEN_HEIGHT * 2) / 3)
-    );
     for (let j = 0; j < 3; j++) {
         for (let i = 0; i < 30; i++) {
             hands.push(
-                new Hand(getRandomPoint(initialCentroids[j], radius), j)
+                new Hand(getRandomPoint(), j)
             );
         }
     }
@@ -39,21 +30,15 @@ function setup() {
 
 function draw() {
     background(0);
-    centroids = computeTargetPositions();
+    updateHandCollections();
     hands.forEach((h) => h.update(1));
 }
 
-function computeTargetPositions() {
-    targetPositions = [];
+function updateHandCollections() {
     rocks = hands.filter((h) => h.type === handType.ROCK);
-    targetPositions.push(computeCentroid(rocks));
     papers = hands.filter((h) => h.type === handType.PAPER);
-    targetPositions.push(computeCentroid(papers));
     scissors = hands.filter((h) => h.type === handType.SCISSOR);
-    targetPositions.push(computeCentroid(scissors));
-
     allHands = [rocks, papers, scissors];
-    return targetPositions;
 }
 
 function computeCentroid(hands) {
@@ -71,13 +56,7 @@ function computeCentroid(hands) {
     }
 }
 
-function getRandomPoint(center, radius) {
-    // let newDir = createVector(Math.random(), Math.random())
-    //     .mult(2)
-    //     .sub(1)
-    //     .mult(radius);
-    // let newPoint = center.copy().add(newDir);
-    // return newPoint;
+function getRandomPoint() {
     return createVector(
         Math.random() * SCREEN_WIDTH,
         Math.random() * SCREEN_HEIGHT
@@ -87,6 +66,5 @@ function getRandomPoint(center, radius) {
 function getNearestNeighbours(hand, hands, n) {
     sorted = hands.slice();
     sorted.sort((a, b) => a.pos.dist(hand.pos) - b.pos.dist(hand.pos));
-    //hands.forEach((h) => console.log(h.pos.dist(hand.pos)));
     return sorted.slice(0, n);
 }
